00e2a74b29a12bebcaaf27bd607e87d44bbf5239
jenia petrusenko <medvijenia@icloud.com>
2025-02-05 00:32:31 +0200

    agents

--- ai/codegen/agents.py
+++ ai/codegen/agents.py
@@ -8,7 +8,7 @@ class CustomAgents:
     def __init__(self) -> None:
         self.config = AzureOpenAIConfig()
 
-    def memory_agent(self):
+    def memory_agent(self) -> Agent:
         return Agent(
             role="Research Specialist",
             goal="Analyze UI elements in an image and generate a test plan.",
@@ -19,3 +19,13 @@ class CustomAgents:
             verbose=True,
             llm=self.config.set_azure_llm,
         )
+
+    def create_test_plan(self) -> Agent: ...
+
+    def map_elements_task(self) -> Agent: ...
+
+    def generate_pytest_code(self) -> Agent: ...
+
+    def code_review_task(self) -> Agent: ...
+
+    def jira_agent_task(self) -> Agent: ...
