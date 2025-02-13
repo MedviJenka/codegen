049ab8bf8f84b944e7a53a9f5319dd9b056aca4e
Jenia Petrusenko <evgeny.petrusenko@audiocodes.com>
2025-02-13 11:24:11 +0200

    functions reader

---
+++ lab/app/src/app/tools/toolkit.py
@@ -0,0 +1,7 @@
+import os
+import ast
+from src.core.paths import GLOBAL_PATH
+
+
+class ToolKit:
+    ...
--- src/core/paths.py
+++ src/core/paths.py
@@ -9,3 +9,4 @@ GLOBAL_PATH: str = abstract_dir.split('src')[0][:-1]
 LOG = fr'{GLOBAL_PATH}\output\code_gen.log'
 PAGE_BASE = fr'{GLOBAL_PATH}\output\page_base.csv'
 PYTHON_CODE = fr'{GLOBAL_PATH}\output\test_code.py'
+FUNCTIONS_INDEX = fr'{GLOBAL_PATH}\functions'
